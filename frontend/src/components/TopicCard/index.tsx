80671342be04bf27114c60e44d24f32717fd80b3
RoniEdu <110207335+RoniEdu@users.noreply.github.com>
2023-08-01 22:05:25 -0300

    aula 01/08/2023

---
+++ frontend/src/components/TopicCard/index.tsx
@@ -0,0 +1,15 @@
+import TopicCardHeader from "../TopicCardHeader"
+import TopicCardBody from "../TopicCardBody"
+import TopicCardActions from "../TopicCardActions";
+
+function TopicCard() {
+    return(
+        <div id="topic-card">
+            <TopicCardHeader/>
+            <TopicCardBody/>
+            <TopicCardActions/>
+        </div>
+    )
+}
+
+export default TopicCard;
---
+++ frontend/src/components/TopicCardBody/index.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+function TopicCardBody() {
+    return(
+        <div id="topic-card-body" style={{marginLeft: '3rem'}}>
+            <Typography variant="body1">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo consectetur adipisci ipsam fugiat nesciunt! 
+                Fugit nam obcaecati enim impedit error placeat, deserunt ad sunt, repudiandae quam, recusandae id veritatis blanditiis?
+            </Typography>
+        </div>
+    )
+}
+
+export default TopicCardBody;
